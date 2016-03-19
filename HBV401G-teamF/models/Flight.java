fcce88ef64f4b4a02a06c071a670f18e78c422ef
oddgeirpall <doddip123@gmail.com>
2016-03-19 16:55:20 +0000

    Flight model ready

--- HBV401G-teamF/models/Flight.java
+++ HBV401G-teamF/models/Flight.java
@@ -1,5 +1,164 @@
 package models;
+import org.eclipse.swt.widgets.DateTime;
 
 public class Flight {
+	// PRIVATE VARIABLES
+	private DateTime DateTime;
+	private int price;
+	private String flightNumber;
+	private String departureLoc;
+	private DateTime departureTime;
+	private String depAirportId;
+	private String arrivalLoc;
+	private DateTime arrivalTime;
+	private String arrAirportId;
+	private String gate;
+	private int numSeats;
+	private int bookedSeats;
+	private int numSagaSeats;
+	private int bookedSagaSeats;
+	private ConnectFlight connectFlight;
+	
+	// CONSTRUCTOR
+	public Flight(DateTime DateTime, int price, String flightNumber, String departureLoc, 
+			DateTime departureTime, String depAirportId, String arrivalLoc, 
+			DateTime arrivalTime, String arrAirportId, String gate, int numSeats, 
+			int bookedSeats, int numSagaSeats, int bookedSagaSeats) {
+		
+		this.DateTime = DateTime;
+		this.price = price;
+		this.flightNumber = flightNumber;
+		this.departureLoc = departureLoc;
+		this.departureTime = departureTime;
+		this.depAirportId = depAirportId;
+		this.arrivalLoc = arrivalLoc;
+		this.arrivalTime = arrivalTime;
+		this.arrAirportId = arrAirportId;
+		this.gate = gate;
+		this.numSeats = numSeats;
+		this.bookedSeats = bookedSeats;
+		this.numSagaSeats = numSagaSeats;
+		this.bookedSagaSeats = bookedSagaSeats;
+		
+	}
+	
+	// PRIVATE METHODS
+	private boolean isConnectedFlight() {
+		// ToDo...
+	}
+	
+	// GET/SET METHODS
+	public DateTime getDateTime() {
+		return DateTime;
+	}
 
+	public void setDateTime(DateTime DateTime) {
+		this.DateTime = DateTime;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+
+	public String getDepartureLoc() {
+		return departureLoc;
+	}
+
+	public void setDepartureLoc(String departureLoc) {
+		this.departureLoc = departureLoc;
+	}
+
+	public DateTime getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(DateTime departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	public String getDepAirportId() {
+		return depAirportId;
+	}
+
+	public void setDepAirportId(String depAirportId) {
+		this.depAirportId = depAirportId;
+	}
+
+	public String getArrivalLoc() {
+		return arrivalLoc;
+	}
+
+	public void setArrivalLoc(String arrivalLoc) {
+		this.arrivalLoc = arrivalLoc;
+	}
+
+	public DateTime getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(DateTime arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public String getArrAirportId() {
+		return arrAirportId;
+	}
+
+	public void setArrAirportId(String arrAirportId) {
+		this.arrAirportId = arrAirportId;
+	}
+
+	public String getGate() {
+		return gate;
+	}
+
+	public void setGate(String gate) {
+		this.gate = gate;
+	}
+
+	public int getNumSeats() {
+		return numSeats;
+	}
+
+	public void setNumSeats(int numSeats) {
+		this.numSeats = numSeats;
+	}
+
+	public int getBookedSeats() {
+		return bookedSeats;
+	}
+
+	public void setBookedSeats(int bookedSeats) {
+		this.bookedSeats = bookedSeats;
+	}
+
+	public int getNumSagaSeats() {
+		return numSagaSeats;
+	}
+
+	public void setNumSagaSeats(int numSagaSeats) {
+		this.numSagaSeats = numSagaSeats;
+	}
+
+	public int getBookedSagaSeats() {
+		return bookedSagaSeats;
+	}
+
+	public void setBookedSagaSeats(int bookedSagaSeats) {
+		this.bookedSagaSeats = bookedSagaSeats;
+	}
+	
+	
 }
