ac40b5f1f6689e69098b4723dec94ba3c6f06584
Stefán Carl Peiser <scg2@hi.is>
2016-03-19 19:56:20 +0000

    Updated UI with a base Search Engine

--- HBV401G-teamF/controllers/SearchEngine.java
+++ HBV401G-teamF/controllers/SearchEngine.java
@@ -1,5 +1,38 @@
 package controllers;
 
-public class SearchEngine {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import models.Flight;
+
+public class SearchEngine {
+	
+	private DatabaseRetriever DBConnection;
+	private ArrayList<Flight> flightList;
+	
+	public SearchEngine(){
+		DBConnection = new DatabaseRetriever();
+	}
+	
+	
+	private void extractDataFromResultSet(ResultSet  results) throws SQLException{
+		flightList = new ArrayList<Flight>();
+		
+		while(results.next()){
+			//saekja dot.		
+			flightList.add(new Flight());			
+		}		
+	}
+	
+	public ArrayList<Flight> searchFlightByCriteria(String departureDate, String returnDate, String departureLocation, String returnLocation, int passengerQty ){
+		
+		
+		ResultSet flightResults = DBConnection.retrieveFlightsByCriteria(departureDate, returnDate, departureLocation, returnLocation, passengerQty);
+		
+		extractDataFromResultSet(flightResults);		
+	
+		return flightList;
+	}
+	
 }
