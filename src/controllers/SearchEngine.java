1dc39a369ebc1ca2d4aea4c291e1040940da0606
Gilly <egill.ian1@gmail.com>
2016-04-03 20:04:41 +0000

    Search engine methods and comparable methods for Flight objects

--- src/controllers/SearchEngine.java
+++ src/controllers/SearchEngine.java
@@ -4,16 +4,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 */
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 import models.Flight;
+import helperFunctions.FlightComparatorAscending;
 import test.DatabaseRetrieverMock;
 
 
 /**
  * @author stefa
+ * The SearchEngine class is not ready yet - so it is
+ * temporarily an interface so JUnit testing can be done
+ * all unfinished methods and such will be commented out
  */
 public class SearchEngine {
 
@@ -21,11 +26,11 @@ public class SearchEngine {
     private ArrayList<Flight> flightList;
     private ArrayList<Flight> filteredFlightList;
     private int passengerQty;
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy")
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
     public SearchEngine(DatabaseRetriever DBConnection){
-		this.DBConnection = DBConnection;
-	}
+        this.DBConnection = DBConnection;
+    }
 
     public ArrayList<Flight> searchFlightByCriteria(String travelDestination, String travelDate, String returnLocation, String returnDate, int passengerQty ){
         flightList = DBConnection.retrieveFlightsByCriteria(travelDestination, travelDate, returnLocation, returnDate, passengerQty);
@@ -41,9 +46,16 @@ public class SearchEngine {
             // Determines if the flight is within specified parameters
             boolean flightEligible = true;
             // Desired date restricitions
-            Date dateFromWanted = dateFormat.parse(dateFrom);
-            Date dateToWanted = dateFormat.parse(dateTo);
-            Date flightDate = dateFormat.parse(flight.getDate());
+            Date dateFromWanted;
+            Date dateToWanted;
+            Date flightDate;
+            try {
+                dateFromWanted = dateFormat.parse(dateFrom);
+                dateToWanted = dateFormat.parse(dateTo);
+                flightDate = dateFormat.parse(flight.getDate());
+            } catch(ParseException e) {
+                System.err.println("String to Date parsing error:" + e.getMessage());
+            }
             // Dates of flight available
             if(dateFromWanted.compareTo(flightDate) >= 0 && dateToWanted.compareTo(flightDate) <= 0){
                 flightEligible = false;
@@ -59,10 +71,20 @@ public class SearchEngine {
                 flightEligible = false;
             }
 
+            // Reject flight if price is too high for client or and client does care about price (price != 0)
+            if(flight.getPrice() > maxPrice && flight.getPrice() != 0){
+                flightEligible = false;
+            }
 
+            filteredFlightList.add(flight);
+        }
 
+        // Determine how to sort list of flights
+        if(priceDescending){
 
         }
+
+        return filteredFlightList;
     }
 
 
