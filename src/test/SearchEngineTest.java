b18c65c919d9d33c03504a580b5f42f51602d38d
Stefán Carl Peiser <scg2@hi.is>
2016-04-03 16:17:50 +0000

    First Commit

---
+++ src/test/SearchEngineTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stefa on 4/3/2016.
+ */
+public class SearchEngineTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+
+    @Test
+    public void testOne() throws Exception{
+
+    }
+}
