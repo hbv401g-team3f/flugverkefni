2fad75b3c5138e9157269d94d574086e80151d48
hildurs <hos18@hi.is>
2016-04-10 16:32:15 +0000

    MySQL tables

--- database_retriever/database_retriever_connectflight.sql
+++ database_retriever/database_retriever_connectflight.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `database_retriever` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `database_retriever`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: database_retriever
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-10 16:22:34
+-- Dump completed on 2016-04-10 16:29:37
