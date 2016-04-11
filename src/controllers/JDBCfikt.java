377dcc4a57306a27d07b5743d3885cd9ce4ca400
Stefán Carl Peiser <scg2@hi.is>
2016-04-11 22:59:53 +0000

    jdbc fikt

---
+++ src/controllers/JDBCfikt.java
@@ -0,0 +1,31 @@
+package controllers;
+
+
+import models.Flight;
+
+import java.util.ArrayList;
+
+/**
+ * Created by stefa on 4/11/2016.
+ */
+public class JDBCfikt {
+
+    public static void main(String[] args) {
+        DatabaseRetriever DBC = new DatabaseRetriever();
+
+        ArrayList<Flight> flightList = new ArrayList<>();
+
+        try {
+            DBC.retrieveFlightsByCriteria("2016-04-20", "Iceland", "Casablanca", 1);
+        } catch (Exception e)
+        {
+            System.err.println("Shiiii "+ e.getMessage());
+        }
+
+        for(Flight currentFlight : flightList){
+            System.out.println("Flight number: "+currentFlight.getFlightNumber()+", Price: " +currentFlight.getPrice()+", Wifi Availablle? "+ currentFlight.getPassengerLuxuries().isWifiAvailable());
+        }
+
+    }
+
+}
